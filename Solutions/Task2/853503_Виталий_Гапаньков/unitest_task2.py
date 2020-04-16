f635c43a23a0dd35871b7849d04782a60d06601d
vgpnkw <62284467+vgpnkw@users.noreply.github.com>
2020-04-16 15:20:30 +0300

    Update unitest_task2.py

--- Solutions/Task2/853503_Виталий_Гапаньков/unitest_task2.py
+++ Solutions/Task2/853503_Виталий_Гапаньков/unitest_task2.py
@@ -1,10 +1,28 @@
 import unittest
 from lab2_task2 import Json
 import json
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         my_json = Json()
         set = [228, False, 14.82, True, {5: "BSUIR1", 0: 28}, [88, 55]]
         self.assertEqual(my_json.to_json(set), json.dumps(set))
 
-unittest.main()
+    def test_1(self):
+        my_json = Json()
+        test = {"vor": 100}
+        self.assertEqual(my_json.to_json(test), json.dumps(test))
+
+    def test_2(self):
+        my_json = Json()
+        test = 100
+        self.assertEqual(my_json.to_json(test), json.dumps(test))
+
+    def test_3(self):
+        my_json = Json()
+        test = {"Univ": "Bsuir", "vor": [3, "str"]}
+        self.assertEqual(my_json.to_json(test), json.dumps(test))
+
+if __name__ == '__main__':
+    unittest.main()
+
