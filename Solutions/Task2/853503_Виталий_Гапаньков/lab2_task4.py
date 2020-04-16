6933c6ccf44b934892832fb4d014cec015d3c433
vgpnkw <62284467+vgpnkw@users.noreply.github.com>
2020-04-16 15:29:12 +0300

    Update lab2_task4.py

--- Solutions/Task2/853503_Виталий_Гапаньков/lab2_task4.py
+++ Solutions/Task2/853503_Виталий_Гапаньков/lab2_task4.py
@@ -1,20 +1,15 @@
 import math
-
-def cached(func):
-    previous_args = [None, None]
-    file = open("result.txt",'a')
-    def wrapper(*args, **kwargs):
-        if(previous_args[0] == args):
-            returned = previous_args[1]
-            file.write(str(returned))
-            return returned
-        else:
-            previous_args.clear()
-            previous_args.append(args)
-            previous_args.append(func(*args, **kwargs))
-            returned = previous_args[1]
-            file.write('\n' + str(returned))
-            return returned
+from functools import wraps
+def cached(function):
+    memo = {}
+    @wraps(function)
+    def wrapper(*args):
+        try:
+            return memo[args]
+        except KeyError:
+            fun = function(*args)
+            memo[args] = fun
+            return fun
     return wrapper
 
 @cached
@@ -25,6 +20,7 @@ def multiplication(a, b):
 @cached
 def plusandpowl(a, b, c):
     return math.pow(a + b, c)
+
 
 multiplication(8,4)
 plusandpowl(2,2,2)
